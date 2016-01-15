84a26fde7e6a056c437332168988c7df4b2d04a6
Daniel Caaz <DCaazy@gmail.com>
2016-01-14 18:20:01 -0800

    Navigation thing actually working

--- Navigation/js/main.js
+++ Navigation/js/main.js
@@ -1,3 +1,16 @@
 $(function(){
-  
+  var $creeper = $('nav>span');
+  $creeper.moveTo = function($target) {
+    $creeper.stop().animate({
+      width:'+='+($target.innerWidth()-$creeper.width())+'px',
+      left:'+='+($target.position().left-$creeper.position().left)+'px'
+    });
+  };
+  $creeper.reset = function() {
+    $creeper.stop().animate({ width:'0px', left:'0px' });
+  }
+  $('#links>a').hover(
+    function(e){ $creeper.moveTo($(e.target)); },
+    function(e){ $creeper.reset(); }
+  );
 });
